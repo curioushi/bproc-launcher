720f124e76ee2fe4aa6e97ace94db3f11fa9c07e
curioushi <curioushi@163.com>
2023-01-16 00:38:24 +0800

    tbd: TUI layout

--- config_setup_app.py
+++ config_setup_app.py
@@ -1,47 +1,48 @@
 from textual.app import App, ComposeResult
 from textual.containers import Container, Vertical, Horizontal
-from textual.widgets import Button, Header, Footer, Static, Label, Input, Checkbox, ListItem, ListView, Tree, TreeNode
+from textual.widgets import Button, Footer, Static, Label, Input, Checkbox, ListItem, ListView, Tree, TreeNode
 from textual import log
 
 
-class MyHeader(Static):
-    def on_mount(self) -> None:
-        self.update('Configuration Setup')
+class AppHeader(Static):
+    def compose(self):
+        self.update("Configuration Setup")
+
+
+class MyVertical(Vertical):
+    pass
+
+
+class MyHorizontal(Horizontal):
+    pass
+
 
 class ConfigSetupApp(App):
-
     CSS_PATH = 'config_setup_app.css'
 
-    def on_button_pressed(self):
-        log.debug('my button mouse down')
-
-    def on_click(self):
-        log.debug('clicked')
-
-    def on_tree_node_selected(self, event):
-        print('node')
-        node: TreeNode = event.node
-        log.debug(node._label)
-        print('node')
-
-
     def compose(self) -> ComposeResult:
-        tree: Tree[dict] = Tree("")
-        tree.root.expand()
-        characters = tree.root.add("Characters", expand=True)
-        characters.add_leaf("Paul")
-        characters.add_leaf("Jessica")
-        characters.add_leaf("Chani")
-        yield Horizontal(tree, 
-                         Container(MyHeader(),
-                                   Label("CC_TEXTURES_DIR"), Input(),
-                                   Label("ENABLE_DEPTH"), Checkbox(),
-                                   Label("SHADING_MODE"), ListView(
-                             ListItem(Static("auto")),
-                             ListItem(Static("smooth")),
-                             ListItem(Static("flat"))),
-                             Button(), classes='option')
-                         )
+        yield Vertical(
+            MyHorizontal(AppHeader(),
+                         Button("Submit")),
+            Horizontal(
+                Tree("Tree"),
+                MyVertical(
+                    MyHorizontal(Label("Param1"), Input()),
+                    MyHorizontal(Label("Param2"), Input()),
+                    MyHorizontal(Label("Param1"), Input()),
+                    MyHorizontal(Label("Param2"), Input()),
+                    MyHorizontal(Label("Param1"), Input()),
+                    MyHorizontal(Label("Param2"), Input()),
+                    MyHorizontal(Label("Param1"), Input()),
+                    MyHorizontal(Label("Param2"), Input()),
+                    MyHorizontal(Label("Param1"), Input()),
+                    MyHorizontal(Label("Param2"), Input()),
+                    MyHorizontal(Label("Param1"), Input()),
+                    MyHorizontal(Label("Param2"), Input()),
+                    MyHorizontal(Label("Param1"), Input()),
+                    MyHorizontal(Label("Param2"), Input()),
+                )
+            ))
 
 
 if __name__ == '__main__':
